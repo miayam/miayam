91d1d4749d2dff804dc99fa44ff027091aaf937b
miayam <muhammaddeni90@gmail.com>
2020-10-26 09:12:12 +0700

    feat(gitignore): Update gitignore.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
-module.exports = (eleventyConfig) => {
+module.exports = (config) => {
+    // Needed to prevent eleventy from ignoring changes to generated
+    // templates since it is in our `.gitignore`
+    config.setUseGitIgnore(false);
 
     return {
         dir: { input: 'src', output: '_site' },
