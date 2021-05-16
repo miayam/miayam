15424c24c6d9e63e1d3a2e44589efc4b31add76c
miayam <muhammaddeni90@gmail.com>
2021-05-16 12:25:21 +0700

    fix: CMS.

--- src/_includes/templates/admin/index.js
+++ src/_includes/templates/admin/index.js
@@ -11,5 +11,19 @@ if (window.netlifyIdentity) {
 }
 
 if (window.CMS) {
+    var ArticlePreview = createClass({
+        render: function() {
+            var entry = this.props.entry;
+            var image = entry.getIn(['data', 'image']);
+            var bg = this.props.getAsset(image);
+            return h('main', {"className": "o-main"}, {},
+                h('h1', {}, entry.getIn(['data', 'title'])),
+                h('img', {src: bg.toString()}),
+                h('div', {"className": "text"}, this.props.widgetFor('body'))
+            );
+        }
+    });
+
+    CMS.registerPreviewTemplate("articles", ArticlePreview);
     CMS.registerPreviewStyle("/styles-admin.css");
 }
