57a1e75729d0a2d6ea2a2e9eef9aa886cfd14fef
miayam <muhammaddeni90@gmail.com>
2021-05-16 12:59:01 +0700

    fix: CMS.

--- src/_includes/templates/admin/index.js
+++ src/_includes/templates/admin/index.js
@@ -4,13 +4,13 @@ if (window.netlifyIdentity) {
     window.netlifyIdentity.on("init", user => {
         if (!user) {
             window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
+                document.location.href = "/admin";
             });
         }
     });
 }
 
-if (window.CMS && window.React) {
+if (window.CMS) {
     const h = React.createElement;
     class ArticlePreview extends React.Component {
         render() {
