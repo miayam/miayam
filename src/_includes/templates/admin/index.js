70dc97a04bc3c70148f2a30e361365e708de21dd
miayam <muhammaddeni90@gmail.com>
2021-05-16 19:32:24 +0700

    fix: CMS.

--- src/_includes/templates/admin/index.js
+++ src/_includes/templates/admin/index.js
@@ -16,14 +16,17 @@ CMS.registerPreviewTemplate("articles", Article);
 CMS.registerPreviewStyle("/styles-blog.css");
 
 // Register any CSS file on the home page as a preview style
-fetch("/")
-  .then(response => response.text())
-  .then(html => {
-    const f = document.createElement("html");
-    f.innerHTML = html;
-    Array.from(f.getElementsByTagName("link")).forEach(tag => {
-      if (tag.rel == "stylesheet" && !tag.media) {
-        CMS.registerPreviewStyle(tag.href);
-      }
-    });
-  });
+var timeout;
+timeout = setTimeout(() => {
+    fetch("/")
+        .then(response => response.text())
+        .then(html => {
+            const f = document.createElement("html");
+            f.innerHTML = html;
+            Array.from(f.getElementsByTagName("link")).forEach(tag => {
+            if (tag.rel == "stylesheet" && !tag.media) {
+                CMS.registerPreviewStyle(tag.href);
+            }
+            });
+        });
+}, 3000);
