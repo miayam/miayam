30e153dee99cc5dde0d4acdcf5d951ac5de52401
Muhammad D. R <muhammaddeni90@gmail.com>
2021-09-29 22:59:03 +0700

    feat: Finish prority+ menu.

--- src/components/molecules/menu/index.js
+++ src/components/molecules/menu/index.js
@@ -1,46 +1,78 @@
 class Menu {
   constructor(className="m-menu") {
     this.className = className;
-  }
-
-  calcWidth() {
+    this.menu = document.getElementsByClassName(this.className)[0];
+    this.tabs = document.getElementsByClassName(`${this.className}__tab`);
+    this.moreTab = document.getElementsByClassName(`${this.className}__moreTab`)[0];
+    this.visibleTabs = document.getElementsByClassName(`${this.className}__tabs`)[0];
+    this.hiddenTabs = document.getElementsByClassName(`${this.className}__hiddenTabs`)[0];
   }
 
   priorityPlus() {
-  }
+    const calcWidth = () => {
+      let tabsWidth = 0; // Total tabs width.
+      const menuWidth = this.menu.offsetWidth;
+      const availableSpace = menuWidth - 45;
 
-  toggle() {
-    const hiddenTabs = document.getElementsByClassName(`${this.className}__hiddenTabs`)[0];
-    hiddenTabs.style.display = 'none';
-    const moreTab = document.getElementsByClassName(`${this.className}__moreTab`)[0];
+      Array.from(this.visibleTabs.childNodes).forEach(tab => {
+        tabsWidth += tab.offsetWidth;
+      });
 
-    const clickOutside = (e) => {
-      if (e.target === hiddenTabs || hiddenTabs.contains(e.target)) {
-        hiddenTabs.style.display = 'block';
-      } else if (e.target === moreTab) {
-        return;
+      if (tabsWidth > availableSpace) {
+        const visibleTabsCount = this.visibleTabs.childElementCount;
+        const lastVisibleTab = this.visibleTabs.childNodes[visibleTabsCount - 1];
+        lastVisibleTab.setAttribute('data-width', lastVisibleTab.offsetWidth);
+        this.hiddenTabs.insertBefore(lastVisibleTab, this.hiddenTabs.childNodes[0]);
+        calcWidth();
       } else {
-        hiddenTabs.style.display = 'none';
+        const firstHiddenTab = this.hiddenTabs.childNodes[0];
+        const firstHiddenTabWidth = Number(firstHiddenTab.getAttribute('data-width'));
+        if (tabsWidth + firstHiddenTabWidth < availableSpace) {
+          this.visibleTabs.appendChild(firstHiddenTab);
+        }
+      }
+
+      if (this.hiddenTabs.childElementCount) {
+        this.moreTab.style.display = 'inline-block';
+      } else {
+        this.moreTab.style.display = 'none';
       }
     };
 
-    moreTab.addEventListener('click', (e) => {
+    window.addEventListener('resize', calcWidth);
+    window.addEventListener('load', calcWidth);
+  }
+
+  toggle() {
+    this.hiddenTabs.style.display = 'none';
+
+    const clickOutside = (e) => {
+      if (e.target === this.hiddenTabs || this.hiddenTabs.contains(e.target)) {
+        this.hiddenTabs.style.display = 'block';
+      } else if (e.target === this.moreTab) {
+        return;
+      } else {
+        this.hiddenTabs.style.display = 'none';
+      }
+    };
+
+    const clickInside = (e) => {
       e.preventDefault();
 
-      if (hiddenTabs.style.display === 'none') {
-        hiddenTabs.style.display = 'block';
+      if (this.hiddenTabs.style.display === 'none') {
+        this.hiddenTabs.style.display = 'block';
       } else {
-        hiddenTabs.style.display = 'none';
+        this.hiddenTabs.style.display = 'none';
       }
-    });
+    };
 
+    this.moreTab.addEventListener('click', clickInside);
     document.addEventListener('click', clickOutside);
   }
 
   init() {
     this.toggle();
-    window.addEventListener('resize', this.priorityPlus);
-    window.addEventListener('load', this.priorityPlus);
+    this.priorityPlus();
   }
 }
 
