97167439501792c1204aa0e29c1a969877e5d461
Muhammad D. R <muhammaddeni90@gmail.com>
2022-04-27 22:02:38 +0700

    fix: Update meta tag.

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,9 +3,9 @@ const main = () => {
     "lang": "en",
     "name": "miayam",
     "title": "The excitement of not knowing",
-    "description": "Not knowing is Buddha. The aha moment is waiting for you.",
+    "description": "Not knowing is Buddha. The Aha! moment is waiting for you.",
     "type": "website",
-    "url": "https://github.com/miayam/miayam",
+    "url": "https://miayam.io",
     "image": "https://avatars2.githubusercontent.com/u/12558753",
     "platforms": ['twitter', 'pinterest', 'telegram', 'tumblr', 'reddit', 'whatsapp', 'linkedin', 'gmail']
   };
